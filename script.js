db3e1d2147f9825b4443b06af1228ad3d156f50a
Mohini Bhimtay <mohinibhimtay1289@gmail.com>
2026-02-13 21:59:52 +0530

    .

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const btn = document.getElementById("openBtn");
 const surprise = document.getElementById("surprise");
+const overlay = document.querySelector(".overlay");
 
 btn.addEventListener("click", () => {
+    overlay.style.display = "none";
     surprise.classList.remove("hidden");
-    btn.style.display = "none";
 });
+
+/* Slideshow */
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}, 3000);
